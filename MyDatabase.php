2725e3c8524ec63e5fc05bac3ed482188614b18e
Lukas Runt <lukas.runt@seznam.cz>
2022-06-22 09:43:43 +0200

    Přidán management uživatelů

--- MyDatabase.php
+++ MyDatabase.php
@@ -7,21 +7,64 @@ class MyDatabase{
 
     private $pdo;
 
-    public function __constructor(){
-        $this->pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    public function __construct(){
+        $this->pdo = new PDO("mysql:host=localhost;dbname=sp_kiv_web", "root", "");
         $this->pdo->exec("set names utf8");
     }
 
-    function getAllUsers(){
-        $q = "SELECT * FROM".TABLE_UZIVATEL."ORDER BY jmeno";
+    /**
+     * It performs a query and either returns the retrieved data, or if an error occurs, it prints it and returns null.
+     *
+     * @param string $query
+     * @return PDOStatement|null
+     */
+    private function executeQuery(string $query){
+        $res = $this->pdo->query($query);
 
-        $res = $this->pdo->query($q);
-
-        if(!$res){
-            return[];
+        if($res){
+            return $res;
         }else{
-            return $res->fetchAll();
+            $error = $this->pdo->errorInfo();
+            echo $error[2];
+            return null;
         }
+    }
+
+    public function selectFromTable(string $tableName, string $whereStatement = "", string $orderStatement = ""):array {
+        $q = "SELECT * FROM ". $tableName
+            .(($whereStatement == "") ? "" : " WHERE $whereStatement")
+            .(($orderStatement == "") ? "" : " ORDER BY $orderStatement");
+
+        $obj = $this->executeQuery($q);
+
+        if($obj == null){
+            return [];
+        }
+
+        return $obj->fetchAll();
+    }
+
+    /**
+     * Function deletes row from table
+     * @param string $tableName name of table
+     * @param string $whereStatement which row will be deleted
+     * @return bool deletion success
+     */
+    public function deleteFromTable(string $tableName, string $whereStatement){
+
+        $q = "DELETE FROM $tableName WHERE $whereStatement";
+
+        $obj = $this->executeQuery($q);
+        if($obj == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    function getAllUsers(){
+        $users = $this->selectFromTable("lrunt_uzivatel", "", "username");
+        return $users;
     }
 }
 
--- user-management.inc.php
+++ user-management.inc.php
@@ -3,8 +3,39 @@
     require_once("MyDatabase.php");
     $db = new MyDatabase();
 
+    //var_dump($users);
+
+    if(isset($_POST['id_uzivatel'])){
+        $res = $db->deleteFromTable("lrunt_uzivatel", "id_uzivatel='$_POST[id_uzivatel]'");
+
+        if($res){
+            echo "OK: uživatel byl smazán z databáze.";
+        }else{
+            echo "ERROR: Smazání uživatele se nezdařilo.";
+        }
+    }
+
     $users = $db->getAllUsers();
 
-    var_dump($users);
-
 ?>
+
+    <div>
+        <b>Správu uživatelů mohou provádět pouze uživatelé s právem Administrátor.</b>
+    </div>
+
+    <h2>Seznam uživatelů</h2>
+    <table border="1">
+        <tr><th>ID</th><th>Username</th><th>E-mail</th><th>Právo</th><th>Smazat</th>
+        <?php
+        // projdu uzivatele a vypisu je
+            foreach ($users as $u) {
+                echo "<tr><td>$u[id_uzivatel]</td><td>$u[username]</td><td>$u[email]</td><td>$u[id_pravo]</td><td>"
+                    ."<form action='' method='POST'>
+                              <input type='hidden' name='id_uzivatel' value='$u[id_uzivatel]'>
+                              <input type='submit' name='potvrzeni' value='Smazat'>
+                          </form>"
+                    ."</td></tr>";
+            }
+        ?>
+    </table>
+
