52714a08f15b39efc32653a9842a3762ca9f11a0
Lukas Runt <lukas.runt@seznam.cz>
2022-10-25 00:18:19 +0200

    confirm dialog for deleting products

--- app/Views/ProductManagementTemplate.php
+++ app/Views/ProductManagementTemplate.php
@@ -41,10 +41,26 @@
                     <td>$product[mnozstvi]</td>
                     <td><button>Uprav</button></td>
                     <td>
-                        <form method='post'>
-                            <input type='hidden' name='id_produkt' value='$product[id_produkt]'>
-                            <button type='submit' class='btn btn-danger' name='action' value='delete'>Smaž</button>
-                        </form>        
+                      <button type='button' class='btn btn-danger' data-toggle='modal' data-target='#exampleModal$product[id_produkt]'>Smazat</button>
+                        <div class='modal fade' id='exampleModal$product[id_produkt]' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                          <div class='modal-dialog' role='document'>
+                            <div class='modal-content'>
+                              <div class='modal-header'>
+                                <h5 class='modal-title' id='exampleModalLabel'>Smazání produktu</h5>
+                              </div>
+                              <div class='modal-body'>
+                                Opravdu chcete smazat produkt <b>$product[nazev]</b>?
+                              </div>
+                              <div class='modal-footer'>
+                                <button type='button' class='btn btn-primary' data-dismiss='modal'>Zrušit</button>
+                                <form method='post'>
+                                      <input type='hidden' name='id_produkt' value='$product[id_produkt]'>
+                                      <button type='submit' class='btn btn-danger' name='action' value='delete'>Smazat</button>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
+                        </div>        
                     </td></tr>";
             }
             $res .= "</tbody>
